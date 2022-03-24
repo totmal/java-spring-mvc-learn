775daa5e7e0c109b073c5c69f40b252ddc4139eb
totmal <joaopaulobsb01@gmail.com>
2022-03-24 16:05:47 -0300

    Alterações feitas e funcionais

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -1,8 +1,14 @@
 package br.com.alura.mvc.mudi.dto;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotBlank;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
 
 public class RequisicaoNovoPedido {
 
@@ -14,8 +20,34 @@ public class RequisicaoNovoPedido {
 	
 	@NotBlank
 	private String urlImagem;
+	
+	@Column(name="DESC", nullable=true, length=1000)
 	private String descricao;
 	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+	
+	private Date dataDaEntrega;
+	private String valorNegociado;
+	
+	public Date getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+	public void setDataDaEntrega(Date dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+	public String getValorNegociado() {
+		return valorNegociado;
+	}
+	public void setValorNegociado(String valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
@@ -46,6 +78,10 @@ public class RequisicaoNovoPedido {
 		pedido.setNomeProduto(nomeProduto);
 		pedido.setUrlImagem(urlImagem);
 		pedido.setUrlProduto(urlProduto);
+		pedido.setDataDaEntrega(dataDaEntrega);
+		pedido.setValorNegociado(valorNegociado);
+				
+		pedido.setStatus(StatusPedido.AGUARDANDO);
 		return pedido;
 	}
 	
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -1,9 +1,10 @@
 package br.com.alura.mvc.mudi.model;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,11 +16,14 @@ public class Pedido {
 	private Long id;
 	
 	private String nomeProduto;
-	private BigDecimal valorNegociado;
-	private LocalDate dataDaEntrega;
+	private String valorNegociado;
+	private Date dataDaEntrega;
 	private String urlProduto;
 	private String urlImagem;
 	private String descricao;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
 
 	public String getNomeProduto() {
 		return nomeProduto;
@@ -29,20 +33,20 @@ public class Pedido {
 		this.nomeProduto = nomeProduto;
 	}
 
-	public BigDecimal getValorNegociado() {
+	public String getValorNegociado() {
 		return valorNegociado;
 	}
 
-	public void setValorNegociado(BigDecimal valorNegociado) {
-		this.valorNegociado = valorNegociado;
+	public void setValorNegociado(String valorNegociado2) {
+		this.valorNegociado = valorNegociado2;
 	}
 
-	public LocalDate getDataDaEntrega() {
+	public Date getDataDaEntrega() {
 		return dataDaEntrega;
 	}
 
-	public void setDataDaEntrega(LocalDate dataDaEntrega) {
-		this.dataDaEntrega = dataDaEntrega;
+	public void setDataDaEntrega(Date dataDaEntrega2) {
+		this.dataDaEntrega = dataDaEntrega2;
 	}
 
 	public String getUrlProduto() {
@@ -67,6 +71,14 @@ public class Pedido {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public StatusPedido getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusPedido status) {
+		this.status = status;
 	}
 
 }
